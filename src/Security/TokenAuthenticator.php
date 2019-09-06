c9524dd646e450372600e966b39467b2be457207
ZinebElam <zineb.elamrani96@gmail.com>
2019-09-06 23:18:08 +0200

    update commit

--- src/Security/TokenAuthenticator.php
+++ src/Security/TokenAuthenticator.php
@@ -1,82 +1,62 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Raphael
- * Date: 01/02/2019
- * Time: 14:27
- */
-
 namespace App\Security;
-
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
-
+use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
 class TokenAuthenticator extends AbstractGuardAuthenticator
 {
     private $em;
-
-    public function __construct(EntityManagerInterface $em)
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        $this->em = $em;
+        $this->em = $entityManager;
     }
-
-
-    public function start(Request $request, AuthenticationException $authException = null)
-    {
-        $data = [
-            'message' => 'Authentication Required'
-        ];
-    }
-
     public function supports(Request $request)
     {
         return $request->headers->has('X-AUTH-TOKEN');
     }
-
     public function getCredentials(Request $request)
     {
         return [
-            'token' => $request->headers->get('X-AUTH-TOKEN'),
+            'token' => $request->headers->get('X-AUTH-TOKEN')
         ];
     }
-
     public function getUser($credentials, UserProviderInterface $userProvider)
     {
         $apiKey = $credentials['token'];
-
-        if(null == $apiKey){
+        if(null === $apiKey) {
             return;
         }
-
-        return $this->em->getRepository(User::class)
-            ->findOneBy(['apiKey'=>$apiKey]);
+        return $this->em->getRepository(User::class)->findOneBy(['apiKey' => $apiKey]);
     }
-
     public function checkCredentials($credentials, UserInterface $user)
     {
         return true;
     }
-
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        return null;
+    }
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
     {
         $data = [
             'message' => strtr($exception->getMessageKey(), $exception->getMessageData())
         ];
+        return new JsonResponse($data, Response::HTTP_FORBIDDEN);
     }
-
-    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    public function start(Request $request, AuthenticationException $authException = null)
     {
-        return null;
+        $data = [
+            'message' => 'Authentication is required'
+        ];
+        return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
     }
-
     public function supportsRememberMe()
     {
         return false;
